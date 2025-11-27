cf6cc254a889e83facae7c9b0f56745db66318a8
Dayasagar543 <dayasagar.543@gmail.com>
2025-11-28 00:40:58 +0530

    added the code of shift register  for the rotational bits

---
+++ shift_register.ino
@@ -0,0 +1,31 @@
+int latch_pin = 11;
+int data_pin = 12;
+int clock_pin = 9;
+int dt = 1000;
+// byte leds=0xFF;
+// byte leds0 = 0b00000000;
+byte leds0 = 0b11111110;
+byte leds1 = 0b10101010;
+byte leds2 = 0b01010101;
+
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(latch_pin, OUTPUT);
+    pinMode(data_pin, OUTPUT);
+    pinMode(clock_pin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly :
+    delay(dt);
+    digitalWrite(latch_pin, LOW);
+    shiftOut(data_pin, clock_pin, LSBFIRST, leds0);
+    digitalWrite(latch_pin, HIGH);
+    //moving right 
+    // leds0=leds0/128+leds0*2;
+    //moving left
+    leds0 = leds0 * 128 + leds0 / 2;
+}
