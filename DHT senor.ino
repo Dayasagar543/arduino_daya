d2f4bfce4cf2bfa1a442a0982fa0855e45a7e28e
Dayasagar543 <dayasagar.543@gmail.com>
2025-12-08 12:00:06 +0530

    changed the code and tested them in real time

--- DHT senor.ino
+++ DHT senor.ino
@@ -1,54 +1,67 @@
-#include<LiquidCrystal.h>
-#include<DHT.h>       
+
+#include <LiquidCrystal.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
 #define DHTTYPE DHT11
 
-int db7=12;
-int db6=11;
-int db5=10;
-int db4=9;
-int enable=8;
-int rs=7;
-int dht=2;
-int dt=2000;
-float temperaturec;
+int db7 = 12;
+int db6 = 11;
+int db5 = 10;
+int db4 = 9;
+int enable = 8;
+int rs = 7;
+int dhtPin = 2;
+int dt = 2000;
+
+float temperatureC;
 float temperatureF;
 float humidity;
 
-LiquidCrystal lcd(rs,enable,db4,db5,db6,db7) ;
-DHT ht(dht,DHTTYPE)
+LiquidCrystal lcd(rs, enable, db4, db5, db6, db7);
+DHT ht(dhtPin, DHTTYPE);
 
-void setup(){
-Serial.begin(9600);
-ht.begin()
-lcd.begin(16, 2);
+void setup() {
+  Serial.begin(9600);
+  ht.begin();
+  lcd.begin(16, 2);
 }
 
-void loop(){
+void loop() {
+  // Print hello first
+  lcd.setCursor(0, 0);
+  lcd.print("Todays");
   lcd.setCursor(0, 1);
-  delay(dt);
-  lcd.print("hello tej");
-  lcd.clear();
-  delay(dt);
-  temperaturec=ht.readTemperature();
-  temperatureF=ht.readTemperature(true);
-  humidity=ht.readHumidity();
-  lcd.setCursor(0, 0)
-  lcd.print("the temperature in centigrade  is");
-  lcd.setCursor(0, 1);
-  lcd.print(temperaturec);
+  lcd.print("weather");
   delay(dt);
   lcd.clear();
-  lcd.setCursor(0, 0)
-  lcd.print("the temperature in Farenite  is");
+
+  // Read sensor data
+  temperatureC = ht.readTemperature();
+  temperatureF = ht.readTemperature(true);
+  humidity = ht.readHumidity();
+
+  // Display temperature in Celsius
+  lcd.setCursor(0, 0);
+  lcd.print("Temp C:");
+  lcd.setCursor(0, 1);
+  lcd.print(temperatureC);
+  delay(dt);
+  lcd.clear();
+
+  // Display temperature in Fahrenheit
+  lcd.setCursor(0, 0);
+  lcd.print("Temp F:");
   lcd.setCursor(0, 1);
   lcd.print(temperatureF);
   delay(dt);
   lcd.clear();
-  lcd.setCursor(0, 0)
-  lcd.print("the temperature in Humidity is");
+
+  // Display humidity
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity:");
   lcd.setCursor(0, 1);
   lcd.print(humidity);
   delay(dt);
   lcd.clear();
-  
 }
