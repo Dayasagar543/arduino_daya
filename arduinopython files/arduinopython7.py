cb76470bfe0cdf8121b0093fa7ae657cef7f23f4
Dayasagar543 <dayasagar.543@gmail.com>
2026-01-09 15:10:05 +0530

    added the v7 files and python arduiono code folder to save python arduion files majorly arduino files

--- arduinopython files/arduinopython7.py
+++ arduinopython files/arduinopython7.py
@@ -3,11 +3,12 @@ import serial
 import time 
 import numpy as np
 
+time.sleep(2)
+#arduino object
+arduino_Data=serial.Serial("COM4",9600)
 
 #null dimentions or position
 origin=0
-#arduino object
-# arduino_Data=serial.Serial("COM4",9600)
 
 # thermometer bulb and the bar for temperature measuremnet
 opacity=0.2
@@ -26,6 +27,19 @@ axis_value=1
 ypos=0.0
 outer_cylinder=cylinder(pos=vector(origin,ypos,origin),color=color.white,length=cylinder_length,radius=outer_cylinder_radius,axis=vector(origin,axis_value,origin),opacity=opacity)
 thermo_cylinder=cylinder(pos=vector(origin,ypos,origin),color=color.red,length=cylinder_length,radius=inner_cylinder_radius,axis=vector(origin,axis_value,origin))
+
 while True :
-    # while arduino_Data.in_waiting==0:
+    while arduino_Data.in_waiting==0:
         pass
+    data_packet=arduino_Data.readline()
+    data_packet = data_packet.decode('utf-8', errors='ignore')
+    data_packet=data_packet.strip("\r\n")
+    data_packet=data_packet.split(" ")
+    temperature_c=data_packet[5]
+    print(temperature_c)
+    temperature_f=data_packet[11]
+    print(temperature_f)
+    humidity=data_packet[15]
+    print(humidity)
+
+    # print(data_packet)
---
+++ arduiono_python_files/vitrual_thermometer_dhtSensor.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+#define dht_pin 2
+#define DHTTYPE DHT11
+float temperaturec;
+float temperaturef;
+float humidity;
+
+DHT dht(dht_pin, DHTTYPE);
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    temperaturec = dht.readTemperature();
+    temperaturef = dht.readTemperature(true);
+    humidity = dht.readHumidity();
+    Serial.print("the temperature in centigrade  is ");
+    Serial.print(temperaturec);
+    Serial.print("the temperature in farenite  is ");
+    Serial.print(temperaturef);
+    Serial.print(" the  humidity  is ");
+    Serial.print(humidity);
+    Serial.println();
+}
