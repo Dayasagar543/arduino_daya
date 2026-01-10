7eb17d94bfbea4e3cdd9fe367fe6b030ea18f021
Dayasagar543 <dayasagar.543@gmail.com>
2026-01-10 12:30:17 +0530

    modified the fil for the requirements

--- arduinopython files/arduinopython7.py
+++ arduinopython files/arduinopython7.py
@@ -3,7 +3,7 @@ import serial
 import time 
 import numpy as np
 
-time.sleep(2)
+time.sleep(1)
 #arduino object
 arduino_Data=serial.Serial("COM4",9600)
 
@@ -16,18 +16,20 @@ opacity=0.2
 #bulb
 outer_bulb_radius=1
 bulb=sphere(pos=vector(origin,origin,origin),color=color.white,radius=outer_bulb_radius,opacity=opacity)
-inner_bulb_radius=0.6
+inner_bulb_radius=0.8
 bulb=sphere(pos=vector(origin,origin,origin),color=color.red,radius=inner_bulb_radius)
 
 #cylinder
-cylinder_length=5
+cylinder_length=6
 outer_cylinder_radius=.5
 inner_cylinder_radius=.3
 axis_value=1
 ypos=0.0
 outer_cylinder=cylinder(pos=vector(origin,ypos,origin),color=color.white,length=cylinder_length,radius=outer_cylinder_radius,axis=vector(origin,axis_value,origin),opacity=opacity)
-thermo_cylinder=cylinder(pos=vector(origin,ypos,origin),color=color.red,length=cylinder_length,radius=inner_cylinder_radius,axis=vector(origin,axis_value,origin))
+temperature_thermo=1
 
+thermo_cylinder=cylinder(pos=vector(origin,ypos,origin),color=color.red,length=temperature_thermo,radius=inner_cylinder_radius,axis=vector(origin,axis_value,origin))
+time.sleep(1)
 while True :
     while arduino_Data.in_waiting==0:
         pass
@@ -36,10 +38,10 @@ while True :
     data_packet=data_packet.strip("\r\n")
     data_packet=data_packet.split(" ")
     temperature_c=data_packet[5]
-    print(temperature_c)
-    temperature_f=data_packet[11]
-    print(temperature_f)
-    humidity=data_packet[15]
-    print(humidity)
-
-    # print(data_packet)
+    print(data_packet)                                                                                         
+    temperature_thermo=float(temperature_c)
+    temperature_f=float(data_packet[11])
+    humidity=float(data_packet[15])
+    print("the temperature is centigrade is "+temperature_c)
+    print("the temperature is farenite is "+ str(temperature_f))
+    print("the humidity is "+str(humidity))
